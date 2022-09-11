36853a044ebfcc13fd606f03800640afe518c05b
MARTINABUON <aartinmbuon@gmail.com>
2022-09-11 14:59:40 -0700

    adds task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in reverse.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(0)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		for (i = 122; i > 96; i--)
+	}
+	putchar('\n');
+	return (0);
+}
