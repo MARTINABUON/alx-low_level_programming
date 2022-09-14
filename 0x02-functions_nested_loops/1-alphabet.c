bdba406fa200204cabc3782f9341661b45b97e9e
MARTINABUON <aartinmbuon@gmail.com>
2022-09-14 08:06:56 -0700

    adds task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - prints alphabet
+*
+* Return: Always 0 (Success)
+*
+*/
+void print_alphabet(void)
+{
+	putchar('a');
+	putchar('\n');
+	
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #ifndef _MAIN_H_
 
 int _putchar(char)
+void print_alphabet(void)
 
 #endif
