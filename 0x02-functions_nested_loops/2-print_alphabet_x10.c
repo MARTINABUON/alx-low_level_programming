e0ef8e7394505f3198e7de62bb28d51dfc6a9280
MARTINABUON <aartinmbuon@gmail.com>
2022-09-14 13:31:56 -0700

    adds task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*print_alphabet_x10- prints alphabet 10 times
+*Return: x10 a-z
+*
+*\
+void print_alphabet_x10(void);
+{
+int h , t;
+
+t = 0;
+while(t < 10)
+	{
+	for (h = 'a'; h <= 'z'; h++);
+		{
+		putchar(h);
+		}
+		t++;
+		putchar('\n');
+	}
+}
+
+
