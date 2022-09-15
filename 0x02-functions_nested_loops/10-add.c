c6c513bb5fedd93e815223ca16b2cf9fc856697e
MARTINABUON <aartinmbuon@gmail.com>
2022-09-15 08:36:57 -0700

    adds task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two numbers
+ * @a - first number input
+ * @b - second number input
+ * Return: a + b
+ *
+ */
+int add(a + b)
+{
+	Return:a + b;
+}	
