c3ed0cf4b0e79f8d2e19f991065f2cbdda8ad5a0
MARTINABUON <aartinmbuon@gmail.com>
2022-09-15 16:00:51 -0700

    adds task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest = a;
 
-	if (a >= b && a >= c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
@@ -24,10 +24,10 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	else 
+	else
 	{
-		largest = b:
+		largest = b;
 	}
-	
+
 	return (largest);
 }
