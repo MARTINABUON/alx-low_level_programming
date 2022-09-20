0079501ee306d174d2a9a19829fd73e277a69e08
MARTINABUON <aartinmbuon@gmail.com>
2022-09-20 14:50:09 -0700

    adds task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,8 @@
 
 /**
  * puts2 - prints every other character of a string starting
- * with the first character, followed by a new line *@str: - string
+ * with the first character, followed by a new line
+ *@str: - string
  * return: void
  *
  */
