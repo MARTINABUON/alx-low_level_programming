1ed69b1211653408fc96dc707535aaddbf01c78a
MARTINABUON <aartinmbuon@gmail.com>
2022-09-20 13:12:26 -0700

    adds task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 *rev_string - Reverse a string
@@ -14,10 +13,10 @@ length = _strlen(s) - 1;
 	while (length > i)
 	{
 	swap_char(s + length, s + i);
-	
+
 	i++;
 	length--;
-	}	
+	}
 }
 
 /**
@@ -28,9 +27,11 @@ length = _strlen(s) - 1;
 int _strlen(char *s)
 {
 	int count, inc;
+
 	inc = 0;
 
 	for (count = 0; s[count] != '\0'; count++)
+
 	inc++;
 
 	return (inc);
