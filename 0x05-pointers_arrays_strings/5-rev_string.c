cbacee0fe7b165112234134feb5903ec8f43d08a
MARTINABUON <aartinmbuon@gmail.com>
2022-09-20 13:02:55 -0700

    adds task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,13 +11,15 @@ void rev_string(char *s)
 int i = 0, length;
 length = _strlen(s) - 1;
 
-while (length > i)
-{
-swap_char(s + length, s + i);
-i++;
-length--;
+	while (length > i)
+	{
+	swap_char(s + length, s + i);
+	
+	i++;
+	length--;
+	}	
 }
-}
+
 /**
  *_strlen - returns the length of a string
  *@s: string
@@ -25,13 +27,13 @@ length--;
  */
 int _strlen(char *s)
 {
-int count, inc;
-inc = 0;
+	int count, inc;
+	inc = 0;
 
-for (count = 0; s[count] != '\0'; count++)
-inc++;
+	for (count = 0; s[count] != '\0'; count++)
+	inc++;
 
-inc++;
+	return (inc);
 }
 
 /**
@@ -42,7 +44,7 @@ inc++;
  */
 void swap_char(char *a, char *b)
 {
-char tmp = *a;
-*a = *b;
-*b = tmp;
+	char tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
