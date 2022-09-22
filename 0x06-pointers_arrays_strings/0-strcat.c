5e4d94992e189b9122caf8066974de87b0af23e9
MARTINABUON <aartinmbuon@gmail.com>
2022-09-22 04:46:55 -0700

    adds task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,9 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int len1, len2;
 	char s1[30] = "dest";
 	char s2[4] = "src";
 
-	strcat(s1, s2);
+	len1 = strlen(s1);
+	len2 = strlen(S2)
+	for (i = 0; i <= len2; i++)
+	{
+		s1[len1 + i] = s2[i];
+	}
 	printf("%s\n", s1);
+retun (dest);
 }
