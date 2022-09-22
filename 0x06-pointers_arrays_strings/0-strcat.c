a51961e1a60d6b336f359ddce6cc88c20ab7169d
MARTINABUON <aartinmbuon@gmail.com>
2022-09-22 03:59:12 -0700

    adds task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates string 
+ * @dest: destination string 
+ * @src: source string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char s1[30] = "dest";
+	char s2[4] = "src";
+	strcat(s1 , s2);
+	printf("%s\n", s1);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef "main.h"
+#define "main.h"
+
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
