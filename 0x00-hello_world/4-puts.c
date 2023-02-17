ca4ac433a2fa8ad5ef8e3b18ce60a106fc681b0f
MARTINABUON <aartinmbuon@gmail.com>
2023-02-16 23:04:38 -0800

    adds task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
  * main - entry function
- * return: Always 0( success)
+ * Return: Always 0( success)
  *
  */
 
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
-
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
