8818f8e26ebb57ea1db040b81c3bf7f8c9a8ab31
MARTINABUON <aartinmbuon@gmail.com>
2023-02-17 00:23:04 -0800

    adds task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,5 +9,7 @@ int main(void)
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	printf("Size of a character: %d byte(s)\n", sizeof(char));
 	printf("Size of an interger: %d byte(s)\n", sizeof(int));
+	printf("Size of long interger: %d byte(s)\n", sizeof(long int));
+	print("Size of a long long interger: %d byte(s)\n", sizeof(long long int));
 	return (0);
 }
