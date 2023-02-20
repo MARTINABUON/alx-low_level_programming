14513b034c9abf3c855857a21f5d2b232a457617
MARTINABUON <aartinmbuon@gmail.com>
2023-02-20 13:57:35 -0800

    adds task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
 *main - entry function
-*Return; 0(Success)
+*Return: 0(Success)
 */
 
 int main(void)
 {
-    int y;
+   int y;
 
-    for (y = 0; y < 10; y++)
-    {   putchar(y % 10 +'0');
-        if (y != 9)
+   for (y = 0; y < 10; y++)
+    {
+	    putchar(y % 10 +'0');
+	    if (y != 9)
         {
             putchar(',');
             putchar(' ');
