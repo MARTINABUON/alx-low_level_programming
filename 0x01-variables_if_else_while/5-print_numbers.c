43319340595e4b9a71523e7f3ad8278e1ff2b269
MARTINABUON <aartinmbuon@gmail.com>
2023-02-20 03:01:36 -0800

    adds task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry function
+ *Description: Prints numbers from 0 to 10
+ *Return: 0(Always success)
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 11; ++m)
+	{
+		printf("%d\n", m);
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 4-print_alphabt.c- prints a-z except e and f
+5-print_numbers.c- prints numbers from 0 to 10
