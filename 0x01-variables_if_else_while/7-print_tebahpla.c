b9c132d31bc5490c99936c82593c3c49234d4463
MARTINABUON <aartinmbuon@gmail.com>
2023-02-20 11:07:06 -0800

    adds atsk 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - entry function
+ *Return: 0(Success)
+ */
+
+int main(void)
+{
+	char t = 'z';
+
+	while (t >= 'a')
+	{
+		putchar(m);
+		t--;
+	}
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 4-print_alphabt.c- prints a-z except e and f
 5-print_numbers.c- prints numbers from 0 to 10
 6-print_numberz.c - prints 0 to 10 using putchar function
+7-print_tebahpla.c- prints alphabets in reverse
