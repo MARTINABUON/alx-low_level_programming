e1b4f383de2bb318991230621db94297062e22fc
MARTINABUON <aartinmbuon@gmail.com>
2023-02-22 12:02:25 -0800

    adds task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,9 +4,17 @@
  * Return: 0
  */
 
-int main(void)
+int main (void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
