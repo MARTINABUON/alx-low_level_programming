e70c1d66a010ac828fbda67ee583ed5e0d8a1a74
MARTINABUON <aartinmbuon@gmail.com>
2023-02-23 07:26:51 -0800

    adds 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * _isupper - checks if an aphabet is uppercase
+ * @c:  uppercase alphabet
+ * Return: return 1 for upper and 0 for lower
+ */
+
+int _isupper(int c)
+{
+	char i;
+	int up;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (i == c)
+			up = 1;
+	}
+	return (upper);
+}
