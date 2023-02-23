ac0e13a7fe6e69ccba380be198daf0feb1364839
MARTINABUON <aartinmbuon@gmail.com>
2023-02-23 05:21:21 -0800

    adds task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 	{
 		for (M = 0; M < 60; M++)
 		{
-			putchar((H / 10) + '0');
+			_putchar((H / 10) + '0');
 			putchar((H % 10) + '0');
 			putchar(':');
 			putchar((M / 10) + '0');
