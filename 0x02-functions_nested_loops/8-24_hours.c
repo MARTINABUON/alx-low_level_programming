f130bca09c59e091b6d59611329f0bf846e5bcae
MARTINABUON <aartinmbuon@gmail.com>
2023-02-23 05:19:35 -0800

    adds task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Check Main
+ * Description: function prints every minute in one day
+ * Return: Nothing
+ */
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			putchar((H / 10) + '0');
+			putchar((H % 10) + '0');
+			putchar(':');
+			putchar((M / 10) + '0');
+			putchar((M % 10) + '0');
+			putchar('\n');
+		}
+	}
+}
