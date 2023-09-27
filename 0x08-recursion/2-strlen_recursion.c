7dc6767fced765e692a11ae954a550f8ce9a29f0
MARTINABUON <aartinmbuon@gmail.com>
2023-09-27 13:56:00 -0700

    adds 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,7 +11,10 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar(*s);
-		return(1 + _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
 	}
 }
