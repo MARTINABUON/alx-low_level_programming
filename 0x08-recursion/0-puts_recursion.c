4d9b42be7d4638cb148fedc65bcc66b38cc3679e
MARTINABUON <aartinmbuon@gmail.com>
2023-09-27 09:08:16 -0700

    Adds task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_puts - Afunctions that prints a string 
+ *@s: The string input
+ *return: Always 0 
+ */
+
+void _puts_recursion(char *s)
+int main()
+{
+	char *s
+	{
+		_putchar(*s);
+		_putchar("\n");
+	}
+}
