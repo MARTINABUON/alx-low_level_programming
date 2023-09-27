44a888c5f8c60bcc06d54e84fedb9392efc55790
MARTINABUON <aartinmbuon@gmail.com>
2023-09-27 12:27:06 -0700

    adds task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- *_puts - Afunctions that prints a string 
+ *_puts_recursion - A functions that prints a string
  *@s: The string input
- *return: Always 0 
+ *return: Always 0
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
