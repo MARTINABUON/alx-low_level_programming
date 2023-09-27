5546284ca113932d0297a1a4866ed8d84f4748a9
MARTINABUON <aartinmbuon@gmail.com>
2023-09-27 15:15:48 -0700

    adds 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,11 +13,15 @@ int _pow_recursion(int x, int y)
 {
 	if (y == 0)
 	{
-		return (x);
+		return (1);
 	}
 	if (y > 0)
 	{
-		return (x * _pow_recursion(x, y - 1));
+		return (_pow_recursion(x, y));
+	}
+	if (x < 0)
+	{
+		return (-_pow_recursion(x, y));
 	}
 	else
 	{
