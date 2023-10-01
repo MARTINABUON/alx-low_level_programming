3422cfef76de647d13a036643f5ce94b5bf069f6
MARTINABUON <aartinmbuon@gmail.com>
2023-09-30 21:28:51 -0700

    adds task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function, argc and argv is passed through the main
+ * @argc: count the number of arguments
+ * @argv: string of arguments in the command line
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,4 @@
 0. It ain't what they call you, it's what you answer to
 	a program that prints its name, followed by a new line.
+1. Silence is argument carried out by other means	
+	A program that prints the number of arguments passed into it.
