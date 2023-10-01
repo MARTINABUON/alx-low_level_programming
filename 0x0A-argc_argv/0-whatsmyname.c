87cf89e7e571e242ad9d9a4b33a79b2a654e081d
MARTINABUON <aartinmbuon@gmail.com>
2023-09-30 21:04:02 -0700

    adds task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function with arguments argc and argv passed thru'
+ * @argc: counts the number of arguments passed
+ * @argv: strings of arguments passed
+ * Return: O(Always success)
+ **/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+}
