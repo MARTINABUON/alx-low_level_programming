819e3d67ecac3ee5eba1d04fced2d8f94a8705ac
MARTINABUON <aartinmbuon@gmail.com>
2023-10-03 10:11:14 -0700

    adds

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,12 +11,12 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
+	arr = malloc(sizeof(char) * size);
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	arr = malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 	{
