8fe3e78e15bb99fb88c76d68af0824da0f326d6f
MARTINABUON <aartinmbuon@gmail.com>
2023-10-07 10:51:17 -0700

    adds 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,24 +1,47 @@
+#include <stdlib.h>
 #include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+
+	return (s);
+}
 #include <stdlib.h>
 /**
  * _calloc - A function that allocates memory for an array, using malloc
  * Return: return NULL if the maloc fails or size = 0, returns
  * pointer ptr if succesful
- */
+ * @size: the size of the array
+ * @nmemb: the bytes for the array
+ *
+ * /
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr;
+	char *ptA;
 
-	ptr =  malloc(sizeof(int) * size);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-	if (nmemb == 0)
-		return (NULL);
-	if (size == 0)
-		return (NULL);
-	if (malloc == NULL)
-		return (NULL);
-	for (nmemb = 0; nmemb < size; nmemb++)
-		ptr[nmemb] = nmemb++;
+	ptA = malloc(size * nmemb);
 
-		return (ptr);
+	if (ptA == NULL)
+		return (NULL);
+
+	_memset(ptA, 0, nmemb * size);
+
+	return (ptA);
 }
