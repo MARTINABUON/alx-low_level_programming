311fbf3ab5d29b4a3d078277f2b13313e57f99f3
MARTINABUON <aartinmbuon@gmail.com>
2023-10-10 04:28:32 -0700

    adds 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure description
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: Dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
